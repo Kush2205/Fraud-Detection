54c4fa10e44709e2d16785ed96363d66409d460a
Kush2205 <kr.kushagra2205@gmail.com>
2025-03-21 20:25:02 +0530

    Production bugs fixed

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-  
     ignoreDuringBuilds: true,
   },
   typescript: {
-    
     ignoreBuildErrors: true,
+  },
+  webpack: (config: any, { isServer }: { isServer: boolean }) => {
+    if (isServer) {
+      config.externals.push({ 'utf-8-validate': 'commonjs utf-8-validate' });
+    }
+    return config;
   },
 }
 
